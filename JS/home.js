3c110bd4c023e4989db73ebb74413b6fffdf3edc
LeCarteloo <filippapiernik1999@gmail.com>
2021-02-02 18:46:18 +0100

    Hamburger + animations
    Added better animations and fixed some minor issues

--- JS/home.js
+++ JS/home.js
@@ -3,5 +3,14 @@ const navLinks = document.querySelector('.nav-links');
 const links = document.querySelectorAll('.navlinks li');
 
 hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle("open");
+        navLinks.classList.toggle("open");
+        hamburger.classList.toggle("open");
 });
+
+function turnOffHamburger(){
+    hamburger.classList.remove("open");
+    navLinks.classList.remove("open");
+}
+
+// If window is resized then class open is being removed
+window.onresize = turnOffHamburger;
