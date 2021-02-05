e294cdaa691a031e3d3864c7c744eb3aae9e556f
LeCarteloo <filippapiernik1999@gmail.com>
2021-02-05 18:56:45 +0100

    Updated hamburger

--- JS/home.js
+++ JS/home.js
@@ -1,15 +1,18 @@
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 const links = document.querySelectorAll('.navlinks li');
+const test = document.querySelector('.test');
 
 hamburger.addEventListener('click', () => {
         navLinks.classList.toggle("open");
         hamburger.classList.toggle("open");
+     //   test.classList.toggle("open");
 });
 
 function turnOffHamburger(){
     hamburger.classList.remove("open");
     navLinks.classList.remove("open");
+   // test.classList.remove("open");
 }
 
 // If window is resized then class open is being removed
