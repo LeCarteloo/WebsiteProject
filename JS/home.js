a890366c745057c93ef9705b3c7052a31771c54e
LeCarteloo <filippapiernik1999@gmail.com>
2021-02-10 18:50:47 +0100

    Hamburger update
    Another hamburger screen update. PC version is finished, mobile versione done in 80%.

--- JS/home.js
+++ JS/home.js
@@ -1,18 +1,18 @@
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 const links = document.querySelectorAll('.navlinks li');
-const test = document.querySelector('.test');
+const menu = document.querySelector('.menu');
 
 hamburger.addEventListener('click', () => {
         navLinks.classList.toggle("open");
         hamburger.classList.toggle("open");
-     //   test.classList.toggle("open");
+     //   menu.classList.toggle("open");
 });
 
 function turnOffHamburger(){
     hamburger.classList.remove("open");
     navLinks.classList.remove("open");
-   // test.classList.remove("open");
+   // menu.classList.remove("open");
 }
 
 // If window is resized then class open is being removed
