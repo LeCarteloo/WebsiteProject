279e26ecb66462b1998481d07d927a6ee6147614
LeCarteloo <filippapiernik1999@gmail.com>
2021-03-04 23:03:48 +0100

    Horizontall Scroll
    Finally working horizontal section scroll. (Working with scroll mouse and arrows)

--- JS/home.js
+++ JS/home.js
@@ -6,14 +6,41 @@ const menu = document.querySelector('.menu');
 hamburger.addEventListener('click', () => {
         navLinks.classList.toggle("open");
         hamburger.classList.toggle("open");
-     //   menu.classList.toggle("open");
+        menu.classList.toggle("open");
 });
 
 function turnOffHamburger(){
     hamburger.classList.remove("open");
     navLinks.classList.remove("open");
-   // menu.classList.remove("open");
+    menu.classList.remove("open");
+}
+var a=1;
+function showCreators(){
+    if(a==1){
+        document.getElementById('creatorsExpand').style.display="inline";
+        return a=0;
+    }
+    else{
+        document.getElementById('creatorsExpand').style.display="none";
+        return a=1;
+    }
+    
 }
 
 // If window is resized then class open is being removed
 window.onresize = turnOffHamburger;
+
+$(".slider section").on('wheel', function(e) {
+    e.preventDefault();        
+    if (e.originalEvent.wheelDelta >= 0) {
+        if ($(this).prev().length) {
+            var prev = "#" + $(this).prev().attr("id");
+            document.querySelector(prev).scrollIntoView({behavior: 'smooth'});
+        }
+    } else {
+        if ($(this).next().length) {
+            var next = "#" + $(this).next().attr("id");
+            document.querySelector(next).scrollIntoView({behavior: 'smooth'});
+        }
+    }
+  });
